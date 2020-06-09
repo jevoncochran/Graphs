6f126ffa6b837ac5570f224bbfa054b99c4bb43c
Jevon Cochran <56050743+jevoncochran@users.noreply.github.com>
2020-06-09 18:11:10 -0300

    Revert "day 1"

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,16 +1,3 @@
 
-from util import Queue
-
 def earliest_ancestor(ancestors, starting_node):
-    q = Queue()
-    q.enqueue(starting_node)
-    # start with starting_node
-    # does it have parents?
-    n = q.dequeue()
-    for item in ancestors:
-        if item[-1] == starting
-    # if it does have parents, get them
-    # does its parents have parents
-    # stop when there are no more "parents"
-    # if there are multiple "parents," return lowest numeric ID
-    # if it does not have parents, return -1
+    pass
